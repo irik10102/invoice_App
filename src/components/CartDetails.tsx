69648faf728d46d85189993a0528ae771a36aaac
irik10102 <sahilghosh10102@gmail.com>
2026-01-21 20:07:39 +0530

    "Only issue is when listed items qty changes it gets reflects on Cart items too"

--- src/components/CartDetails.tsx
+++ src/components/CartDetails.tsx
@@ -21,9 +21,6 @@ const CartDetails = (props:Prop)=>{
     
     var {cart, setCart} = props;
 
-    const rem = (i:number)=>{
-        setCart(i);
-    }
 
     return(
         <>
@@ -34,16 +31,21 @@ const CartDetails = (props:Prop)=>{
                     <th>Total-Price</th>
                 </tr>
                 {cart.map((val,ind)=>{
-                    if(val.added){
                         const {name, price,category, qty} = val;
-                        return  <tr>
-                                    <td>{name}</td>
-                                    <td>{category}</td>
-                                    <td>{qty*price}/-</td>
-                                    <td><button onClick={()=>{rem(ind)}}>Remove</button></td>
-                                </tr>
-                    }
+                        return  (
+                                        <tr>
+                                            <td>{name}</td>
+                                            <td>{category}</td>
+                                            <td>{qty*price}/-</td>
+                                            <td><button onClick={()=>{setCart(ind)}}>Remove</button></td>
+                                        </tr>
+                                        
+                                )
                 })}
+                <tr><td><p><b>Grand Total:</b>{cart.reduce((acc,val)=>{
+                    acc = acc + (val.price*val.qty);
+                    return acc;
+                },0)}</p></td></tr>
             </table>
         </>
     )
--- src/components/ItemInfo.tsx
+++ src/components/ItemInfo.tsx
@@ -6,6 +6,7 @@ import CartDetails from "./CartDetails";
 
 
 interface Item{
+    index:number,
     name:string,
     price:number,
     category:string,
@@ -19,17 +20,21 @@ const ItemInfo = ()=>{
     const refCat   = useRef<HTMLSelectElement>(null);
 
     const [list, setList] = useState<Item[]>([]);
+    const [cartList,setCartList] = useState<Item[]>([]);
+    var indexG:number = 0;                                         //global index
 
     
 
-    const listItem = ()=>{
+    const listItem = ()=>{                                       //Adds new Item into the list
         const name = refName.current!.value;
         const price = refPrice.current!.value;
         const cat = refCat.current!.value;
 
-        setList([...list, {['name']:name, ['price']:Number(price), ['category']:cat, ['qty']:1, ['added']:false}]);
+        setList([...list, {['index']:indexG,['name']:name, ['price']:Number(price), ['category']:cat, ['qty']:1, ['added']:false}]);
+        indexG+=1;
     }
 
+    //Both inc and dec are updating the list
     const inc = (ind:number)=>{
         list[ind].qty +=1;
         setList([...list]);
@@ -42,33 +47,24 @@ const ItemInfo = ()=>{
     }
 
     const addCart = (ind:number)=>{
-        
-            list[ind].added = true;
-            setList([...list]);
-        
+        setCartList([...cartList, list[ind]]);           //we capture the state at which we want to keep it in list
     }
 
-    const cartUpd = (ind:number)=>{
-        list[ind].added = false;
+    const remList = (ind:number)=>{                      //Removing from list and updating with setList()
+        list.splice(ind,1);
         setList([...list]);
     }
 
-    const rem = (i:number)=>{
-        list.splice(i,1);
+    const cartUpd = (i:number)=>{                        //This function updates the cartList, so that the element in list could be added again the its "added" property is falsed.
+        list[cartList[i].index].added = false;
         setList([...list]);
+
+        cartList.splice(i,1);
+        setCartList([...cartList]);
+
+        
     }
 
-
-    const countCartItems = ()=>{
-        return list.reduce((acc,val)=>{
-            if(val.added)
-                acc += 1;
-            return acc;
-        },0)
-    }
-
-    let countCart = countCartItems();
-    console.log(countCartItems()); 
     return(
         <>
             <table className="container mt-5" >
@@ -116,13 +112,15 @@ const ItemInfo = ()=>{
                                                         <td ><button onClick={()=>inc(ind)}>+</button></td>
                                                         <td>{qty}</td>
                                                         <td><button onClick={()=>dec(ind)}>-</button></td>
-                                                        <td >{(!val.added)?<button onClick={()=>addCart(ind)}>Add Cart</button>:<p><b>Added!</b></p>}</td>
-                                                        <td><button onClick={()=>{rem(ind)}}>Remove</button></td>
+                                                        <td >{(!val.added)?<button onClick={()=>addCart(ind)}>Add to Cart</button>:<p><b>Added!</b></p>}</td>
+                                                        <td><button onClick={()=>{remList(ind)}}>Remove</button></td>
                                                     </tr>
                     
                 })}</table>:<p><b>No Items Added yet!</b></p>}
 
-                {(countCart)?<CartDetails cart={list} setCart={cartUpd}/>:<p><b>No Items Added to cart yet!</b></p>}
+                {(cartList.length > 0)?<CartDetails cart={cartList} setCart={cartUpd}/>:<p><b>No Items Added to cart yet!</b></p>}
+
+                
                     
                     
             </div>
